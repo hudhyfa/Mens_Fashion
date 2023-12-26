dbf21e0c7a67c41b8fb9a9dfeebf2595e85c55e3
hudhyfa <hudhyfahbk@gmail.com>
2023-12-26 13:00:52 +0530

    made category unique

--- server/controller/categoryController.js
+++ server/controller/categoryController.js
@@ -24,18 +24,17 @@ const add_category = async (req,res) => {
 
         if(category_name && category_desc){
 
-            const category_exists = await Category.findOne({category_name: category_name})
+            const category_exists = await Category.findOne({name : category_name})
 
             if(category_exists){
                 req.flash('catExists',"Category already exists")
-                return res.status(402).redirect('/categories')
+                return res.status(402).redirect('/add_category')
             }else{
                 const new_category = await Category.create({
                     name: category_name,
                     description: category_desc
                 })
                 await new_category.save();
-                console.log(`Added category ${new_category}.`)
 
                 return res.status(200).redirect('/categories')
             }

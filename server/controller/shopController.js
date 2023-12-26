d4b46a0885017c7f75047b3a14f19ba888a40195
hudhyfa <hudhyfahbk@gmail.com>
2023-12-26 11:55:11 +0530

    pagination added to product listing

--- server/controller/shopController.js
+++ server/controller/shopController.js
@@ -2,8 +2,21 @@ const Product = require('../modal/product')
 
 const shop_products = async (req,res) => {
     try {
-        const products = await Product.find().populate({path:"category",select:"name"})
-        res.render('user/shop',{products:products})
+        const page = parseInt(req.query.page) || 1;
+        const limit = 1;
+        const skip = (page - 1)*limit;
+
+        const products = await Product.find()
+            .populate({path:"category",select:"name"})
+            .limit(limit)
+            .skip(skip)
+
+        const totalProducts = await Product.countDocuments(); // count of products
+
+        const totalPages = Math.ceil(totalProducts / limit); // total number of pages
+
+        res.render('user/shop',{products:products, currentPage:page, totalPages})
+
     } catch (error) {
         throw new Error('error while rendering shopping page: ' + error)
     }
@@ -22,6 +35,7 @@ const view_product = async (req,res) => {
             console.error('product not found',error)
             return res.status(403).redirect('/show-products')
         }
+
     } catch (error) {
         throw new Error('error while viewing product: ' + error)
     }

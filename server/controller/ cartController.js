ef3d66a72d328f94fca93fb2221669dc4404dc80
hudhyfa <hudhyfahbk@gmail.com>
2024-01-02 19:56:47 +0530

    improved logic of add to cart

--- server/controller/ cartController.js
+++ server/controller/ cartController.js
@@ -14,138 +14,99 @@ const get_cart = async (req,res) => {
     }
 }
 
-const add_to_cart = async (req,res) => {
+const add_to_cart = async (req, res) => {
     try {
-
-        const user_id = req.session.userId;
-        const product_id = req.params.id;
-
-        console.log(req.body);
-        const { price,size } = req.body;
-
-        if (!user_id || !product_id || !price || !size) {
-            throw new Error('Invalid input'); // Handle invalid input
-        }
-      
-
-        if(user_id){
-
-            const [cart,product] = await Promise.all([
-                Cart.findOne({user_id: user_id}),
-                Product.findOne({product_id: product_id})
-            ])
-
-            if(!cart){
-                console.log("creating new cart and adding to cart");
-                const new_cart = await Cart.create({
-                    user_id: user_id,
-                    products:[{
-                        product_id: product_id,
-                        quantity: 1,
-                        size:size,
-                        product_total: price
-                    }],
-                    cart_total: price
-                })
-                await new_cart.save()
-                console.log("created and added cart item");
-
-            }else{
-                console.log("cart already exists");
-                // const productIndexWithSize = cart.products.findIndex(p => p.product_id === product_id && p.size === size);
-            
-                // if(productIndexWithSize !== -1){
-                //     console.log("item exist in the cart with same size");
-                    // const check_stock = product.stock.find(s => s.size === size);
-                    // if(check_stock && check_stock.quantity > 0){
-                    //     await Cart.updateOne(
-                    //         {
-                    //             _id:user_id,
-                    //             'products.product_id':product_id,
-                    //             'products.$.size':size
-                    //         },
-                    //         {
-                    //             $inc:{
-                    //                 'products.$.quantity': 1,
-                    //                  cart_total: price
-                    //             }
-                    //         }
-                    //     )
-                    //     console.log("quantity updated and cart_total too");
-                    // }
-                const { matchedIndex, cart } = await Cart.findOneAndUpdate(
-                    {
-                        _id: user_id,
-                        "products.product_id": product_id,
-                        "products.size": size
-                    },
-                    {
-                        $inc: {
-                            // Check stock using check_stock variable
-                            "products.$.quantity": {
-                                $cond: {
-                                    if: { $gt: [check_stock.quantity, 0] }, // Use check_stock.quantity
-                                    then: 1,
-                                    else: 0
-                                }
-                            },
-                            cart_total: {
-                                $cond: {
-                                    if: { $gt: [check_stock.quantity, 0] }, // Use check_stock.quantity
-                                    then: price,
-                                    else: 0
-                                }
-                            }
-                        }
-                    },
-                    {
-                        new: true
-                    }
-                );
-
-                if(matchedIndex !== -1){
-                    if(cart.products[matchedIndex].quantity > 0){
-                        console.log("updated quantity and cart_total for existing item");
-                    }else{
-                        console.log("stock unavailable for the size or item");
-                    }
-                }else{
-                    console.log("item doesn't exist adding new");
-                    const new_cart_item = {
-                        product_id: product_id,
-                        quantity: 1,
-                        size:size,
-                        product_total: price
-                    }
-
-                    await Cart.updateOne(
-                        {user_id:user_id},
-                        {
-                            $push:{
-                                products:new_cart_item
-                            },
-                            $inc:{
-                                cart_total:price
-                            }
-                        }
-                    )
-                    
-                    await cart.save();
-                    console.log("added new item to cart and updated cart total");
-                    
-                }
-                
+      const user_id = req.session.userId;
+      const product_id = req.params.id;
+      const { size } = req.body;
+      const price = parseInt(req.body.price);
+  
+      if (!user_id || !product_id || !price || !size) {
+        throw new Error('Invalid input');
+      }
+  
+      const [cart, product] = await Promise.all([
+        Cart.findOne({ user_id: user_id }),
+        Product.findOne({ _id: product_id })
+      ]);
+  
+      if (!cart) {
+        // Create a new cart
+        const new_cart = await Cart.create({
+          user_id: user_id,
+          products: [{
+            product_id: product_id,
+            quantity: 1,
+            size: size,
+            product_total: price
+          }],
+          cart_total: price
+        });
+        await new_cart.save();
+        console.log("created and added cart item");
+        res.status(200).redirect(`/view-product/${product_id}`);
+      } else {
+        // Cart exists, check for product and stock
+        const check_stock = product.stock.find(s => s.size === size);
+        const productIndexWithSize = cart.products.findIndex(p => p.product_id.equals(product_id) && p.size === size);
+        console.log(cart.products)
+        console.log("index",productIndexWithSize)
+        if (check_stock.quantity > 0 && productIndexWithSize !== -1) {
+          // Increment quantity of existing item
+          await Cart.findOneAndUpdate(
+            {
+              user_id: user_id,
+              "products.product_id": product_id,
+              "products.size": size
+            },
+            {
+              $inc: {
+                "products.$.quantity": 1,
+                cart_total: price
+              }
+            },
+            {
+              new: true
             }
+          );
+  
+          console.log("quantity of the item updated in the cart");
+          res.status(200).redirect(`/view-product/${product_id}`);
+        } else if (check_stock.quantity > 0) {
+          // Add new item to cart
+          const new_cart_item = {
+            product_id: product_id,
+            quantity: 1,
+            size: size,
+            product_total: price
+          };
+  
+          await Cart.updateOne(
+            { user_id: user_id },
+            {
+              $push: {
+                products: new_cart_item
+              },
+              $inc: {
+                cart_total: price
+              }
+            }
+          );
+  
+          console.log("added new item to cart and updated cart total");
+          res.status(200).redirect(`/view-product/${product_id}`);
+        } else {
+          // Item out of stock
+          console.log("item out of stock");
+          res.status(404).redirect(`/view-product/${product_id}`);
         }
-    
-        
-
-    } catch(error) {
-        console.error("error while adding item to cart",error);
-        res.status(500).send("error adding item to cart");
+      }
+    } catch (error) {
+      console.error("error while adding item to cart", error);
+      res.status(500).send("error adding item to cart");
     }
-
-}
+  };
+  
 
 module.exports = {
     get_cart,
